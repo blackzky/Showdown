2fbb1a8e7224e73a0fb0f088079f4bb88929ee90
Gervel Giva <gervelgiva@gmail.com>
2013-06-20 23:15:08 +0800

    Added kinetic js and the menu state, some basic stats

--- js/main.js
+++ js/main.js
@@ -1,46 +1,139 @@
 var Device = {
-		  width : function(){
-					 if (window.innerWidth){
-								return window.innerWidth;
-					 }else if (document.body && document.body.offsetWidth){
-								return document.body.offsetWidth;
-					 }else{
-								return 0;
-					 }
-		  },
-		  height : function(){
-					 if (window.innerHeight){
-								return window.innerHeight;
-					 }else if (document.body && document.body.offsetHeight){
-								return document.body.offsetHeight;
-					 }else{
-								return 0;
-					 }
-		  }
+	width : function(){
+		if (window.innerWidth){
+			return window.innerWidth;
+		}else if (document.body && document.body.offsetWidth){
+			return document.body.offsetWidth;
+		}else{
+			return 0;
+		}
+	},
+	height : function(){
+		if (window.innerHeight){
+			return window.innerHeight;
+		}else if (document.body && document.body.offsetHeight){
+			return document.body.offsetHeight;
+		}else{
+			return 0;
+		}
+	}
 };
+var setScreen = function(){
+	stage.setWidth(Device.width());
+	stage.setHeight(Device.height());
+	stage.setWidth(document.querySelector("body").clientWidth);
+	stage.setHeight(document.querySelector("body").clientHeight - 10);
 
-var tellMeTheSizes=function(){
-		  document.getElementById("viewportwidth").innerHTML = Device.width() + "px";
-		  document.getElementById("viewportheight").innerHTML = Device.height() + "px";
-		  document.getElementById("resolutionheight").innerHTML = screen.height + "px";
-		  document.getElementById("resolutionwidth").innerHTML = screen.width + "px";
+	if(typeof(WebSettings) != 'undefined'){ WebSettings.setBuiltInZoomControls(false); }
+	window.scrollTo(0, 1);
+}
+function writeMessage(messageLayer, message) {
+	var context = messageLayer.getContext();
+	messageLayer.clear();
+	context.font = '18pt Calibri';
+	context.fillStyle = 'black';
+	context.fillText(message, 10, 25);
 }
 
-window.onload=function(){
-		  tellMeTheSizes();
+var stage;
+var OBJ;
+var messageLayer;
+var circle;
 
-		  if(typeof(WebSettings) != 'undefined'){
-					 WebSettings.setBuiltInZoomControls(false);
-		  }
-		  window.scrollTo(0, 1);
+var CURRENT_STATE = "menu";
+var STATES = {};
+
+function initStage(){
+	stage = new Kinetic.Stage({
+		container: 'container',
+		width: Device.width(),
+		height: Device.height()
+	});
+
+	STATES.MENU = {
+		NAME : "menu",
+		LAYER : new Kinetic.Layer(),
+		INIT : function(){
+			var playButton = new Kinetic.Label({
+				x: (stage.getWidth() / 2),
+				y: (stage.getHeight() / 2),
+				opacity: 0.75
+			});
+			playButton.add(new Kinetic.Tag({
+			  fill: 'teal',
+				stroke: 'black'
+			})).add(new Kinetic.Text({
+			  text: 'Play',
+			  fontFamily: 'Arial',
+			  fontSize: 30,
+			  padding: 5,
+			  fill: 'black'
+			})).on('mouseup touchend', function(e) {
+				writeMessage(messageLayer, "Button");
+			});
+			STATES.MENU.LAYER.add(playButton);
+		}
+	};
+	STATES.MENU.INIT();
+
+	var circleLayer = new Kinetic.Layer();
+	messageLayer = new Kinetic.Layer();
+
+	circle = new Kinetic.Circle({
+		x: (stage.getWidth() / 2),
+		y: (stage.getHeight() / 2),
+		radius: 70,
+		fill: 'red',
+		stroke: 'black',
+		strokeWidth: 4
+	});
+
+	circle.on('mousedown touchstart', function(e) {
+		OBJ = this;
+		writeMessage(messageLayer, "Circle: mousedown or touchstart");
+	});
+
+	circle.on('mouseup touchend', function(e) {
+		writeMessage(messageLayer, "Circle: moveout and touchend");
+	});
+
+	circleLayer.add(circle);
+
+	//stage.add(circleLayer);
+	stage.add(STATES.MENU.LAYER);
+	stage.add(messageLayer);
+
+	document.addEventListener('mousedown', inputDown, false);
+	document.addEventListener('touchstart', inputDown, false);
+
+	setScreen();
 }
 
-window.onresize=function(){
-		  tellMeTheSizes();
+var clear;
+function inputDown(event){
+	event.preventDefault();
+	var object = OBJ ? OBJ : circle;
+
+	if(typeof(event.touches) == 'undefined'){
+		console.log("Document: mousedown");
+		object.setX(event.x);
+		object.setY(event.y);
+	}else{
+		console.log("Document: touchstart");
+		for (var i = 0; i < event.touches.length; i++) {
+			var touch = event.touches[i];
+			object.setX(touch.pageX);
+			object.setY(touch.pageY);
+		}
+	}
+	stage.draw();
 }
 
-window.onmousemove=function(event){
-		  ev = event || window.event;
-		  document.getElementById("mousetop").innerHTML = ev.pageY + "px";
-		  document.getElementById("mouseleft").innerHTML = ev.pageX + "px";
+window.onload = function(){
+	initStage();
 }
+
+window.onresize = function(){
+	setScreen();
+}
+
