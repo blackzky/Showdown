d8e04e53132bab4fabe07554c01b37c1913bb4cf
Gervel Giva <gervelgiva@gmail.com>
2013-06-24 18:10:20 +0800

    Added shape icons -initial commit

--- js/StateHandler.js
+++ js/StateHandler.js
@@ -45,16 +45,16 @@ var StateHandler = {
 			},
 			init : function(){
 				this.layers.field = new Kinetic.Layer();
-				var polySize = 32;
+				var polySize = 64;
 				
 				var polygonContainer = new Kinetic.RegularPolygon({
 					x: 0,
 					y: 0,
 					sides: 8,
-					radius: polySize,
+					radius: polySize/2,
 					fill: 'red',
 					stroke: 'black',
-					strokeWidth: 0,
+					strokeWidth: 3,
 					rotationDeg: 22.5,
 					name: "polygonContainer"
 				}).on("mousedown", function(event){
@@ -65,12 +65,13 @@ var StateHandler = {
 					this.draw();
 				});
 
+				var padding = 5;
 				var p1s = [];
-				var p1sBaseX = polySize;
+				var p1sBaseX = polySize/2 + padding;
 				for(var i = 0; i < 6; i++){
-					var p1sX = (i > 2) ? StageHandler.stage.getWidth() - polySize : polySize;
+					var p1sX = (i > 2) ? StageHandler.stage.getWidth() - (polySize/2) - padding : (polySize/2) + padding;
 					var m = (i > 2) ? i - 3 : i;
-					var p1sY = p1sBaseX + (polySize*2*m);
+					var p1sY = p1sBaseX + (polySize*m);
 					p1s[i] = polygonContainer.clone();
 					p1s[i].setX(p1sX);
 					p1s[i].setY(p1sY);
@@ -79,11 +80,11 @@ var StateHandler = {
 				}
 
 				var p2s = [];
-				var p2sBaseX = StageHandler.stage.getHeight() - (polySize * 5);
+				var p2sBaseX = StageHandler.stage.getHeight() - (polySize * 2.5) - padding;
 				for(var i = 0; i < 6; i++){
-					var p2sX = (i > 2) ? StageHandler.stage.getWidth() - polySize : polySize;
+					var p2sX = (i > 2) ? StageHandler.stage.getWidth() - (polySize/2) - padding : (polySize/2) + padding;
 					var m = (i > 2) ? i - 3 : i;
-					var p2sY = p2sBaseX + (polySize*2*m);
+					var p2sY = p2sBaseX + (polySize*m);
 					p2s[i] = polygonContainer.clone();
 					p2s[i].setFill("blue");
 					p2s[i].setX(p2sX);
@@ -92,15 +93,68 @@ var StateHandler = {
 					this.layers.field.add(p2s[i]);
 				}
 
-				var p1Sup = new Kinetic.Rect({
-					x: 0,
-					y: 0,
-					width: 70,
-					height: 70,
+				var lifeIco = new Kinetic.Path({
+					x: p1s[0].getX() - polySize/2.9,
+					y: p1s[0].getY() - polySize/4,
+					data: 'M248.078,5.883c-36.691-14.739-77.771-0.839-98.517,31.125C128.817,5.044,87.735-8.856,51.043,5.883C9.354,22.632-10.863,70.009,5.887,111.696c16.06,39.98,143.314,139.607,143.314,139.607l0.359,0.28l0.36-0.28c0,0,127.251-99.627,143.314-139.607C309.986,70.009,289.768,22.632,248.078,5.883z',
 					fill: 'green',
+					scale: 0.15,
 					stroke: 'black',
-					strokeWidth: 0
+					strokeWidth: 10
 				});
+
+				this.layers.field.add(lifeIco);
+
+				var reflectIco = new Kinetic.Rect({
+				  x: p1s[1].getX() - polySize/4,
+				  y: p1s[1].getY() - polySize/4,
+				  width: polySize/2,
+				  height: polySize/2,
+				  fill: 'green',
+				  stroke: 'black',
+				});
+				this.layers.field.add(reflectIco);
+
+				var shieldIco = new Kinetic.RegularPolygon({
+				  x: p1s[2].getX(),
+				  y: p1s[2].getY(),
+				  sides: 3,
+				  radius: polySize/3,
+				  fill: 'green',
+				  stroke: 'black',
+				});
+				this.layers.field.add(shieldIco);
+
+				var normalIco = new Kinetic.Circle({
+				  x: p1s[3].getX(),
+				  y: p1s[3].getY(),
+				  radius: polySize/3,
+				  fill: 'green',
+				  stroke: 'black',
+				});
+				this.layers.field.add(normalIco);
+
+				var fastIco = new Kinetic.RegularPolygon({
+				  x: p1s[4].getX(),
+				  y: p1s[4].getY(),
+				  sides: 5,
+				  radius: polySize/3,
+				  fill: 'green',
+				  stroke: 'black',
+				});
+				this.layers.field.add(fastIco);
+
+				var pierceIco = new Kinetic.RegularPolygon({
+				  x: p1s[5].getX(),
+				  y: p1s[5].getY(),
+				  sides: 8,
+				  radius: polySize/3,
+				  fill: 'green',
+				  stroke: 'black',
+				});
+				this.layers.field.add(pierceIco);
+
+
 				var circle = new Kinetic.Circle({
 					//x: (StageHandler.stage.getWidth() / 3),
 					//y: (StageHandler.stage.getHeight() / 2),
