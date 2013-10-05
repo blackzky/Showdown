6a4ab03c4389b730e8b1774a4d431b127e1ccb65
Gervel Giva <gervelgiva@gmail.com>
2013-10-06 00:21:44 +0800

    Added another character

--- js/GameObjects.js
+++ js/GameObjects.js
@@ -1,4 +1,3 @@
-
 var Character = function(config){
 	var id = config.id || "character";
 	var position = [(config.position[0] || 0), (config.position[1]) || 0];
@@ -13,6 +12,11 @@ var Character = function(config){
 	var layer = config.layer || null; 
 	var gameController = config.gameController || null;
 
+	var player = config.player || "player";
+	var color = config.color || "black";
+	var rotationDeg = config.rotationDeg || 0;
+	var direction = config.direction || DIRECTION.CENTER;
+
 	var sprite = new Kinetic.Sprite({
 		x: position[0], y: position[1],
 		offset: offset,
@@ -25,63 +29,78 @@ var Character = function(config){
 		multitouch: true,
 		listening: false,
 		id: id, 
+		rotationDeg: rotationDeg
+	});
+
+	var playerName = new Kinetic.Text({
+		x: sprite.getX() - (player.length*7.5/2),
+		y: sprite.getY() + (sprite.getHeight()/2),
+		text: player,
+		fill: color 
 	});
 
 	layer.add(sprite);
+	layer.add(playerName);
 
 	this.Sprite = sprite;
 
-	var direction = DIRECTION.CENTER;
 
 	this.update = function(){
 		if(gameController){
 			direction = gameController.getDirection();
-
-			switch(direction){
-				case DIRECTION.NORTHWEST:
-					sprite.setRotationDeg(315);
-					break;
-				case DIRECTION.NORTH:
-					sprite.setRotationDeg(0);
-					break;
-				case DIRECTION.NORTHEAST:
-					sprite.setRotationDeg(45);
-				case DIRECTION.EAST:
-					sprite.setRotationDeg(90);
-					break;
-				case DIRECTION.SOUTHEAST:
-					sprite.setRotationDeg(135);
-					break;
-				case DIRECTION.SOUTH:
-					sprite.setRotationDeg(180);
-					break;
-				case DIRECTION.SOUTHWEST:
-					sprite.setRotationDeg(225);
-					break;
-				case DIRECTION.WEST:
-					sprite.setRotationDeg(270);
-					break;
-				default:
-					sprite.setRotationDeg(0);
-					break;
-			}
+			rotateSprite(direction);
 
 			if(gameController.isUsed()){
-				character = sprite;
-				var deg = character.getRotation();
+				var deg = sprite.getRotation();
 
 				var speed = 96;
 				var delta = Game.delta;
-				var vx = character.getX() + (delta * speed * Math.sin(deg));
-				var vy = character.getY() - (delta * speed * Math.cos(deg));
+				var vx = sprite.getX() + (delta * speed * Math.sin(deg));
+				var vy = sprite.getY() - (delta * speed * Math.cos(deg));
 				if(vx < 0){ vx = Game.screenWidth(); }else if(vx > Game.screenWidth()){ vx = 0;	}
 				if(vy < 0){ vy = Game.screenHeight(); }else if(vy > Game.screenHeight()){ vy = 0;	}
 
-				character.setPosition([vx, vy]);
-				character.getLayer().draw();
+				sprite.setPosition([vx, vy]);
+				resetNamePosition();
+
+				layer.draw();
 			}
 
 		}
 
 	};
+
+	var rotateSprite = function(direction){
+		switch(direction){
+			case DIRECTION.NORTHWEST:
+				sprite.setRotationDeg(315);
+				break;
+			case DIRECTION.NORTH:
+				sprite.setRotationDeg(0);
+				break;
+			case DIRECTION.NORTHEAST:
+				sprite.setRotationDeg(45);
+			case DIRECTION.EAST:
+				sprite.setRotationDeg(90);
+				break;
+			case DIRECTION.SOUTHEAST:
+				sprite.setRotationDeg(135);
+				break;
+			case DIRECTION.SOUTH:
+				sprite.setRotationDeg(180);
+				break;
+			case DIRECTION.SOUTHWEST:
+				sprite.setRotationDeg(225);
+				break;
+			case DIRECTION.WEST:
+				sprite.setRotationDeg(270);
+				break;
+		}
+	};
+
+	var resetNamePosition = function(){
+		var x = sprite.getX() - (player.length*7.5/2);
+		var y = sprite.getY() + (sprite.getHeight()/2);
+		playerName.setPosition([x, y]);	
+	}
 };
--- js/States.js
+++ js/States.js
@@ -20,6 +20,12 @@ menu.setInit(function(){
 		layer: this.layers.hud
 	});
 
+	var controllerTwo = new GameController({
+		id: "controllerTwo",
+		position: [Game.screenWidth() - 160, Game.screenHeight() - 160],
+		layer: this.layers.hud
+	});
+
 	var cw = 64, ch = 64;
 	var animations = {
 		idle: [
@@ -36,112 +42,43 @@ menu.setInit(function(){
 	};
 
 	var characterOne = new Character({
-		position: [Game.screenWidth()/2 - cw/2, Game.screenHeight()/2 - ch/2],
+		position: [96, Game.screenHeight()/2 - ch/2],
 		offset: [cw/2, ch/2],
 		width: 64, height: 64,
 		image: Game.Sprites.character,
 		animation: 'idle',
 		animations: animations,
-		id: "character",
+		id: "characterOne",
 		layer: this.layers.hud,
-		gameController: controllerOne
+		gameController: controllerOne,
+		player: "ken",
+		color: "blue",
+		rotationDeg: 90, 
+		direction: DIRECTION.EAST
+	});
+
+	var characterTwo = new Character({
+		position: [Game.screenWidth() - 96, Game.screenHeight()/2 - ch/2],
+		offset: [cw/2, ch/2],
+		width: 64, height: 64,
+		image: Game.Sprites.character,
+		animation: 'idle',
+		animations: animations,
+		id: "characterTwo",
+		layer: this.layers.hud,
+		gameController: controllerTwo,
+		player: "luke",
+		color: "red",
+		rotationDeg: 270,
+		direction: DIRECTION.WEST
 	});
 
 	Game.addEntity(characterOne);
-
+	Game.addEntity(characterTwo);
 
 	//this.layers.hud.add(simpleText);
 	//blob.setAnimation("walk"); 
 	//blob.start();
 });
 
-
-
-var Character = function(config){
-	var id = config.id || "character";
-	var position = [(config.position[0] || 0), (config.position[1]) || 0];
-	var width = config.width || 0;
-	var height = config.height || 0;
-	var offset = [config.offset[0] || 0, config.offset[1] || 0];
-	var image = config.image || Game.Sprites.character; 
-	var animation = config.animation || "idle";
-	var animations = config.animations || null;
-	var frameRate = config.frameRate || 7;
-	var animationIndex = config.animationIndex || 0;
-	var layer = config.layer || null; 
-	var gameController = config.gameController || null;
-
-	var sprite = new Kinetic.Sprite({
-		x: position[0], y: position[1],
-		offset: offset,
-		width: width, height: height,
-		image: image,
-		animation: animation,
-		animations: animations,
-		frameRate: frameRate,
-		index: animationIndex,
-		multitouch: true,
-		listening: false,
-		id: id, 
-	});
-
-	layer.add(sprite);
-
-	this.Sprite = sprite;
-
-	var direction = DIRECTION.CENTER;
-
-	this.update = function(){
-		if(gameController){
-			direction = gameController.getDirection();
-
-			switch(direction){
-				case DIRECTION.NORTHWEST:
-					sprite.setRotationDeg(315);
-					break;
-				case DIRECTION.NORTH:
-					sprite.setRotationDeg(0);
-					break;
-				case DIRECTION.NORTHEAST:
-					sprite.setRotationDeg(45);
-				case DIRECTION.EAST:
-					sprite.setRotationDeg(90);
-					break;
-				case DIRECTION.SOUTHEAST:
-					sprite.setRotationDeg(135);
-					break;
-				case DIRECTION.SOUTH:
-					sprite.setRotationDeg(180);
-					break;
-				case DIRECTION.SOUTHWEST:
-					sprite.setRotationDeg(225);
-					break;
-				case DIRECTION.WEST:
-					sprite.setRotationDeg(270);
-					break;
-				default:
-					sprite.setRotationDeg(0);
-					break;
-			}
-
-			if(gameController.isUsed()){
-				character = sprite;
-				var deg = character.getRotation();
-
-				var speed = 96;
-				var delta = Game.delta;
-				var vx = character.getX() + (delta * speed * Math.sin(deg));
-				var vy = character.getY() - (delta * speed * Math.cos(deg));
-				if(vx < 0){ vx = Game.screenWidth(); }else if(vx > Game.screenWidth()){ vx = 0;	}
-				if(vy < 0){ vy = Game.screenHeight(); }else if(vy > Game.screenHeight()){ vy = 0;	}
-
-				character.setPosition([vx, vy]);
-				character.getLayer().draw();
-			}
-
-		}
-
-	};
-
-};
 
