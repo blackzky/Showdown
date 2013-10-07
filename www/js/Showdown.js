084c39c76380d458c383dec0ee279d5fbe16e8bd
Gervel Giva <gervelgiva@gmail.com>
2013-10-07 12:22:19 +0800

    phone gap update 0.1 -test

--- www/js/Showdown.js
+++ www/js/Showdown.js
@@ -1,6 +1,8 @@
 var move_char_t = false;
 var use_delta = true;
 window.onload = function(){
+	app.initialize();
+
 	Game.addSource("dPadContainer", "img/d-pad-container.png"); //combine the to image to a single image <Controller>
 	Game.addSource("dPadCenter", "img/d-pad-center.png");
 	Game.addSource("character", "img/character.png");
@@ -11,21 +13,12 @@ window.onload = function(){
 
 	Game.addState("Menu", menu);
 
-	handleEvents();
-
 	Game.setUpdate(function(){
 
 	});
 
 	Game.start({container :"container"});
 }
-
-/* Code to handle events like mouse, touch and keyboard*/
-function handleEvents(){
-
-
-}
-
 
 
 window.onresize = function(){
