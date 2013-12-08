c49aa79001bf8a41c22420102c37b67c95a8fb02
blackzky <gervelgiva@gmail.com>
2013-12-08 19:36:37 +0800

    Update Showdown.js

--- www/js/Showdown.js
+++ www/js/Showdown.js
@@ -1,25 +1,27 @@
 var move_char_t = false;
 var use_delta = true;
-window.onload = function(){
-	app.initialize();
-
-	Game.addSource("dPadContainer", "img/d-pad-container.png"); //combine the to image to a single image <Controller>
+var app = {
+    initialize: function(){
+        this.bindEvents()
+   }, bindEvents: function(){
+        document.addEventListener('deviceready', this.onDeviceReady, false); 
+    }, onDeviceReady: function(){
+        app.receiveEvent('deviceready'); 
+        
+        Game.addSource("dPadContainer", "img/d-pad-container.png"); 
 	Game.addSource("dPadCenter", "img/d-pad-center.png");
 	Game.addSource("character", "img/character.png");
 	Game.addSource("box", "img/box.png");
 	Game.addSource("circle", "img/circle.png");
 	Game.addSource("x", "img/x.png");
 	Game.addSource("triangle", "img/triangle.png");
-
 	Game.addState("Menu", menu);
-
-	Game.setUpdate(function(){
-
-	});
-
 	Game.start({container :"container"});
+    } 
+};
+window.onload = function(){
+	app.initialize();
 }
-
 
 window.onresize = function(){
 	Game.setScreen();
